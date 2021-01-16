4e68b08d1ed991c54d9d4e3cf88792d91edd9c69
Nestor Cortina <junior110120@gmail.com>
2021-01-16 10:22:51 -0500

    refactor: http

--- src/interfaces/httpInterfaces.ts
+++ src/interfaces/httpInterfaces.ts
@@ -2,3 +2,8 @@ export interface ResponseWS<T extends any> {
   status: number
   data: T
 }
+
+export enum MESSAGES_HTTP {
+  NETWORK_ERROR = 'NO TIENES INTERNET',
+  DEFAULT_ERROR = 'ERROR TECNICO'
+}
